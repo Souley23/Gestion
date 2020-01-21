d5393ea0694cc178c840e3934bb01e9be2f0055b
Souley23 <57152135+Souley23@users.noreply.github.com>
2020-01-21 03:02:25 +0100

    Delete Moderator.blade.php

--- resources/views/Moderator.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.moderator')
-@section('moderator')
-<div class="contenaire">
-  <div class="monitor">
-    <img src="{{asset('images/inscription.png')}}"
-  </div>
-</div>
-@endsection
